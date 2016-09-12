7d20e2d0cf8ba9712c0256e9c4cf19e5ab3197aa
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-12 15:34:48 -0400

    Hard coded data for Slogan generator. Added buttons for individual noun/ adjective generation.

--- makeSlogansGreat/src/Generate.js
+++ makeSlogansGreat/src/Generate.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
+import Results from './Results';
 
 class Generate extends Component {
-  submitQuery(evt){
+  constructor(props){
+    super(props)
+    this.state = {
+      hasGenerated: false,
+    }
+  }
+  submitNounQuery(evt){
+    evt.preventDefault();
+    console.log("Make nouns noun-ier");
+  }
+  submitSloganQuery(evt){
     evt.preventDefault();
     var clicks = ["Make clicking great again!", "I am the best most luxurious clicker you've ever seen.", "If Ivanka weren't my daughter perhaps we'd be clicking", "I will be the greatest clicking president that God ever created", "I have the best clicks"]
     console.log(clicks[Math.floor(Math.random()*clicks.length)]);
+    this.setState({
+      hasGenerated: true,
+    });
+  }
+  submitAdjectiveQuery(evt){
+    evt.preventDefault();
+    console.log("I love the poorly educated");
   }
   render(){
+    let words = [
+      {content: "nouns"},
+      {content: "adjective-y"}
+    ]
+    if (this.state.hasGenerated){
+      return (
+        <Results words={words} smh={["alkjsdf", 'lkjasdf']}/>
+      )
+    }
     return(
       <div>
-        <form onSubmit={(evt) => this.submitQuery(evt)}>
+        <form onSubmit={(evt) => this.submitNounQuery(evt)}>
+          <button type="submit" className="trumpton-sm center-block"> Make Nouns Great!</button>
+        </form>
+
+        <form onSubmit={(evt) => this.submitSloganQuery(evt)}>
           <button type="submit" className="trumpton center-block"> Make Slogans Great!</button>
+        </form>
+
+        <form onSubmit={(evt) => this.submitAdjectiveQuery(evt)}>
+          <button type="submit" className="trumpton-sm center-block"> Make Adjectives Great!</button>
         </form>
       </div>
     )
---
+++ makeSlogansGreat/src/Results.js
@@ -0,0 +1,26 @@
+import React, {Component} from "react"
+
+class Results extends Component{
+  render(){
+    console.log(this.props);
+    let {words} = this.props;
+    console.log(words);
+    let results = words.map((word, index) => {
+      return (
+        <div key={index}>
+          {word.content}
+        </div>
+      )
+    })
+
+    return (
+      <div>
+      Make
+      {results}
+      Again
+      </div>
+    )
+  }
+}
+
+export default Results
