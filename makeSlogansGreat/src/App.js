1f6e3f67dd941efa326ad39537f953f3d77ec2ef
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-12 09:49:06 -0400

    Hello world

---
+++ makeSlogansGreat/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>Jello World!</div>
+    )
+  }
+}
+
+export default HelloWorld;
