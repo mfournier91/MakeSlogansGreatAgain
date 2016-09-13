decb36831c744241260170632f551a84fca1998c
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-09-13 10:25:00 -0400

    Slogans are capitalized.

--- makeSlogansGreat/src/Utils.js
+++ makeSlogansGreat/src/Utils.js
@@ -3,14 +3,17 @@ import $ from 'jquery';
 export function queryApi() {
   var nounUrl = 'http://localhost:8080/nouns';
   var adjUrl = 'http://localhost:8080/adjectives'
-  return $.getJSON(nounUrl).then(function(nouns){
+  return $.getJSON(nounUrl).then(function(nouns){ //query first api
     var noun = nouns[Math.floor(Math.random()*nouns.length)];
-    noun["content"] = " " + noun["content"] + " ";
-    return $.getJSON(adjUrl).then(function(adjectives){
-      var adjective = adjectives[Math.floor(Math.random()*adjectives.length)];
-      adjective["content"] = " " + adjective["content"] + " ";
 
-      var response = [noun, adjective];
+    //put a space and capitalize first letter
+    noun["content"] = " " + noun["content"].charAt(0).toUpperCase() + noun["content"].slice(1) + " ";
+    return $.getJSON(adjUrl).then(function(adjectives){  //query second
+      var adjective = adjectives[Math.floor(Math.random()*adjectives.length)];
+
+      adjective["content"] = " " + adjective["content"].charAt(0).toUpperCase() + adjective["content"].slice(1) + " ";
+
+      var response = [noun, adjective]; //return both
       console.log(response);
       return response;
     })
